98a08ecfd7fa8d472ef3ef22ad1db3e1db6b4163
utah iev <uiev@csod.com>
2017-07-26 14:21:17 -0700

    Added Basic Authentication Middleware

---
+++ csod-edge-integrations-custom-provider-service/Middleware/BasicAuthenticationHandler.cs
@@ -0,0 +1,63 @@
+﻿using csod_edge_integrations_custom_provider_service.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csod_edge_integrations_custom_provider_service.Middleware
+{
+
+    public class BasicAuthenticationHandler : AuthenticationHandler<BasicAuthenticationOptions>
+    {
+        public BasicAuthenticationHandler()
+        {
+        }
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {            
+            var authHeader = (string)this.Request.Headers["Authorization"];
+
+            if (!string.IsNullOrEmpty(authHeader) && authHeader.StartsWith("basic", StringComparison.OrdinalIgnoreCase))
+            {
+                //Extract credentials
+                string encodedUsernamePassword = authHeader.Substring("Basic ".Length).Trim();
+                Encoding encoding = Encoding.GetEncoding("iso-8859-1");
+                string usernamePassword = encoding.GetString(Convert.FromBase64String(encodedUsernamePassword));
+
+                int seperatorIndex = usernamePassword.IndexOf(':');
+
+                var username = usernamePassword.Substring(0, seperatorIndex);
+                var password = usernamePassword.Substring(seperatorIndex + 1);
+
+                if (username == "test" && password == "test")
+                {
+                    var user = new GenericPrincipal(new GenericIdentity("test"), null);
+                    var ticket = new AuthenticationTicket(user, new AuthenticationProperties(), Options.AuthenticationScheme);
+                    return Task.FromResult(AuthenticateResult.Success(ticket));           
+                }
+                else
+                {
+
+                    //var dbUser = _userContext.Users.SingleOrDefault(x => x.Username.Equals(username, StringComparison.OrdinalIgnoreCase));
+                    //if (dbUser != null)
+                    //{
+                    //    if (dbUser.Password.Equals(password))
+                    //    {
+                    //        var user = new CustomPrinciple(dbUser);
+                    //        var ticket = new AuthenticationTicket(user, new AuthenticationProperties(), Options.AuthenticationScheme);
+                    //        return Task.FromResult(AuthenticateResult.Success(ticket));
+                    //    }
+                    //}
+                }
+
+                return Task.FromResult(AuthenticateResult.Fail("No valid user."));
+            }
+
+            this.Response.Headers["WWW-Authenticate"] = "Basic realm=\"yourawesomesite.net\"";
+            return Task.FromResult(AuthenticateResult.Fail("No credentials."));
+        }
+    }
+}
---
+++ csod-edge-integrations-custom-provider-service/Middleware/BasicAuthenticationMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using csod_edge_integrations_custom_provider_service.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace csod_edge_integrations_custom_provider_service.Middleware
+{
+    public class BasicAuthenticationMiddleware : AuthenticationMiddleware<BasicAuthenticationOptions>
+    {
+        public BasicAuthenticationMiddleware(
+           RequestDelegate next,
+           IOptions<BasicAuthenticationOptions> options,
+           ILoggerFactory loggerFactory,
+           UrlEncoder encoder)
+           : base(next, options, loggerFactory, encoder)
+        {            
+        }
+
+        protected override AuthenticationHandler<BasicAuthenticationOptions> CreateHandler()
+        {
+            return new BasicAuthenticationHandler();
+        }
+    }
+}
