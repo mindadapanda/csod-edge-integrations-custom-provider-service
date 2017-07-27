62c74bbbc3da34d8099d9ddb8a7df58fbcc20379
minda <mwang@csod.com>
2017-07-27 13:35:21 -0700

    fixed controllers to be more secure and commented CRUD operations for both users and settings, they are now access more securely through the UI instead of an open endpoint

--- csod-edge-integrations-custom-provider-service/Controllers/UserController.cs
+++ csod-edge-integrations-custom-provider-service/Controllers/UserController.cs
@@ -39,107 +39,107 @@ namespace csod_edge_integrations_custom_provider_service.Controllers
             return View();
         }
 
-        [Route("api/users")]
-        [HttpGet]
-        public IActionResult GetAllUsers()
-        {
-            var users = UserRepository.GetAll();
+        //[Route("api/users")]
+        //[HttpGet]
+        //public IActionResult GetAllUsers()
+        //{
+        //    var users = UserRepository.GetAll();
 
-            return Ok(users);
-        }
+        //    return Ok(users);
+        //}
 
-        [Route("api/user/{id}")]
-        [HttpGet]
-        public IActionResult GetUser(int id)
-        {
-            var user = UserRepository.GetUser(id);
-            if (user != null)
-            {
-                return Ok(user);
-            }
-            return NotFound();
-        }
+        //[Route("api/user/{id}")]
+        //[HttpGet]
+        //public IActionResult GetUser(int id)
+        //{
+        //    var user = UserRepository.GetUser(id);
+        //    if (user != null)
+        //    {
+        //        return Ok(user);
+        //    }
+        //    return NotFound();
+        //}
 
-        [Route("api/user")]
-        [HttpPost]
-        public IActionResult CreateUser([FromBody]User user)
-        {
-            try
-            {
-                if (string.IsNullOrWhiteSpace(user.Username)
-                    || string.IsNullOrWhiteSpace(user.Password))
-                {
-                    return BadRequest();
-                }
-                //make username unqiue
-                var users = UserRepository.GetAll();
-                if(users.Any(x => x.Username.Equals(user.Username)))
-                {
-                    return BadRequest();
-                }
-                //make sure to salt and hash the user password
-                user.Password = UserTool.GenerateSaltedHash(user.Password);
+        //[Route("api/user")]
+        //[HttpPost]
+        //public IActionResult CreateUser([FromBody]User user)
+        //{
+        //    try
+        //    {
+        //        if (string.IsNullOrWhiteSpace(user.Username)
+        //            || string.IsNullOrWhiteSpace(user.Password))
+        //        {
+        //            return BadRequest();
+        //        }
+        //        //make username unqiue
+        //        var users = UserRepository.GetAll();
+        //        if(users.Any(x => x.Username.Equals(user.Username)))
+        //        {
+        //            return BadRequest();
+        //        }
+        //        //make sure to salt and hash the user password
+        //        user.Password = UserTool.GenerateSaltedHash(user.Password);
 
-                UserRepository.CreateUser(user);
-            }
-            catch
-            {
-                return BadRequest();
-            }
+        //        UserRepository.CreateUser(user);
+        //    }
+        //    catch
+        //    {
+        //        return BadRequest();
+        //    }
 
-            return new NoContentResult();
-        }
+        //    return new NoContentResult();
+        //}
 
-        [Route("api/user/{id}")]
-        [HttpPut]
-        public IActionResult UpdateUser(int id, [FromBody]User updatedUser)
-        {
-            if(string.IsNullOrWhiteSpace(updatedUser.Username) 
-                || string.IsNullOrWhiteSpace(updatedUser.Password)
-                || id != updatedUser.Id)
-            {
-                return BadRequest();
-            }
-            var user = UserRepository.GetUser(id);
-            if(user == null)
-            {
-                return BadRequest();
-            }
-            //make sure they don't update the username
-            if(!user.Username.Equals(updatedUser.Username))
-            {
-                return BadRequest();
-            }
-            //make sure to salt and hash the user password
-            updatedUser.Password = UserTool.GenerateSaltedHash(updatedUser.Password);
-            UserRepository.UpdateUser(updatedUser);
-            return new NoContentResult();
-        }
+        //[Route("api/user/{id}")]
+        //[HttpPut]
+        //public IActionResult UpdateUser(int id, [FromBody]User updatedUser)
+        //{
+        //    if(string.IsNullOrWhiteSpace(updatedUser.Username) 
+        //        || string.IsNullOrWhiteSpace(updatedUser.Password)
+        //        || id != updatedUser.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+        //    var user = UserRepository.GetUser(id);
+        //    if(user == null)
+        //    {
+        //        return BadRequest();
+        //    }
+        //    //make sure they don't update the username
+        //    if(!user.Username.Equals(updatedUser.Username))
+        //    {
+        //        return BadRequest();
+        //    }
+        //    //make sure to salt and hash the user password
+        //    updatedUser.Password = UserTool.GenerateSaltedHash(updatedUser.Password);
+        //    UserRepository.UpdateUser(updatedUser);
+        //    return new NoContentResult();
+        //}
 
-        [Route("api/user/{id}")]
-        [HttpDelete]
-        public IActionResult DeleteUser(int id)
-        {
-            UserRepository.DeleteUser(id);
-            return new NoContentResult();
-        }
+        //[Route("api/user/{id}")]
+        //[HttpDelete]
+        //public IActionResult DeleteUser(int id)
+        //{
+        //    UserRepository.DeleteUser(id);
+        //    return new NoContentResult();
+        //}
 
-        [Route("api/user/login")]
-        [HttpPost]
-        public IActionResult Login([FromBody]UserLoginRequest loginRequest)
-        {
-            if (string.IsNullOrWhiteSpace(loginRequest.Username)
-                || string.IsNullOrWhiteSpace(loginRequest.Password))
-            {
-                return BadRequest();
-            }
-            var user = UserRepository.GetUserByUsername(loginRequest.Username);
-            if(UserTool.DoPasswordsMatch(loginRequest.Password, user.Password))
-            {
-                return Ok(user);
-            }
-            return BadRequest();
-        }
+        //[Route("api/user/login")]
+        //[HttpPost]
+        //public IActionResult Login([FromBody]UserLoginRequest loginRequest)
+        //{
+        //    if (string.IsNullOrWhiteSpace(loginRequest.Username)
+        //        || string.IsNullOrWhiteSpace(loginRequest.Password))
+        //    {
+        //        return BadRequest();
+        //    }
+        //    var user = UserRepository.GetUserByUsername(loginRequest.Username);
+        //    if(UserTool.DoPasswordsMatch(loginRequest.Password, user.Password))
+        //    {
+        //        return Ok(user);
+        //    }
+        //    return BadRequest();
+        //}
 
         [Route("api/getuserandsettings")]
         [HttpPost]
