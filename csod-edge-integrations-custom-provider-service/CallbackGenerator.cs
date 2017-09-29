7991fdcc9d77cd0355c7846ca1cb4dfde39a21b6
utah iev <uiev@csod.com>
2017-09-29 11:18:58 -0700

    More cleanup

---
+++ csod-edge-integrations-custom-provider-service/CallbackGenerator.cs
@@ -0,0 +1,28 @@
+﻿using csod_edge_integrations_custom_provider_service.Data;
+using csod_edge_integrations_custom_provider_service.Models;
+using System;
+
+namespace csod_edge_integrations_custom_provider_service
+{
+    public class CallbackGenerator
+    {
+        CallbackRepository _callbackRepository;
+        public CallbackGenerator(CallbackRepository callbackRepository)
+        {
+            _callbackRepository = callbackRepository;
+        }
+        public Guid GenerateCallback(string url, int callbackLimit = 10)
+        {
+            Guid guid = Guid.NewGuid();
+            var callback = new Callback()
+            {
+                PublicId = guid,
+                EdgeCallbackUrl = url,
+                Limit = callbackLimit
+            };
+
+            _callbackRepository.CreateCallback(callback);
+            return guid;
+        }
+    }
+}
